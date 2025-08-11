a1ecbfd3b64e3fedca059b285db6cfc618c971b1
Areen <areenmasri072@gmail.com>
2025-08-11 14:23:21 +0300

    Update

--- models.py
+++ models.py
@@ -8,24 +8,16 @@ from database import Base
 
 print("Base from database =", Base)
 
-class Admin(Base):
-    __tablename__ = "admins"
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True, index=True)
-    password_hash = Column(String(100))  
-  
-
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True, index=True)
-    password_hash = Column(String(100))
+    username = Column(String(50))
     posts = relationship("Post", back_populates="owner")
-    
+
 class Post(Base):
     __tablename__ = "posts"
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(100))     
-    content = Column(String(255))   
+    title = Column(String(50))
+    content = Column(String(50))
     user_id = Column(Integer, ForeignKey("users.id"))
     owner = relationship("User", back_populates="posts")
